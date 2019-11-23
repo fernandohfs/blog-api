d2c4f51714c9e247d537b2c48ebe1b0b6d9d390a
fernandohfs <fernando.hfs94@gmail.com>
2019-11-23 12:48:37 -0300

    add firts route

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
 import Hapi from "@hapi/hapi";
+import HapiRouter from "hapi-router";
 
 class Server {
   constructor() {
@@ -10,8 +11,25 @@ class Server {
 
   init() {
     this.server.start();
+    this._plugins();
     console.log(`Server running on ${this.server.info.uri}`);
   }
+
+  async _plugins() {
+    await this.server.register([
+      {
+        plugin: HapiRouter,
+        options: {
+          routes: "src/api/**/**/**.routes.js"
+        }
+      }
+    ]);
+  }
 }
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
 
 new Server().init();
